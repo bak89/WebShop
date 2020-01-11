7472bae1719f8747fee78312cde6bb888c332947
Giorgio <gbakhiet@gmail.com>
2020-01-11 16:46:54 +0100

    edit and delete button all language

--- view/templates/home.php
+++ view/templates/home.php
@@ -12,23 +12,25 @@
     $name = $product->getProductName();
     $price = $product->getProductPrice();
 
-    echo "<a id=\"product-page\" href='index.php?action=product_page&id=$id'>";
-    //id?
+    echo "<a id=\"product-page\" href=\"" . $this->build_url('index.php', array('action' => 'product_page', 'id' => $id)) . " \">";
+
     echo "<div class=product-container>";
 
     echo "<p class='name' class=\"prod-name\">$name</p>";
     ?>
     <div class='image-container'>
         <?php
-    echo "<img class='prod-img' src='assets/images/$image'>";
-    ?>
+        echo "<img class='prod-img' src='assets/images/$image'>";
+        ?>
     </div>
-    <?php
-    echo "<p class='price'>CHF $price</p>";
+    <?php echo "<p class='price'>CHF $price</p>";
 
-    if ($this->controller->isAdmin()) {
-        echo "<br><a href=\"index.php?action=edit_product&id=$id\">Edit</a> | <a href=\"index.php?action=delete_product&id=$id\">Delete</a><br/>";
-    }
+    if ($this->controller->isAdmin()) echo
+        "<a href=\"" . $this->build_url('index.php', array('action' => 'edit_product','id'=>$id)) . " \">
+                    <button>Edit</button>
+                </a> | <a href=\"" . $this->build_url('index.php', array('action' => 'delete_product','id'=>$id)) . " \">
+                    <button>Delete</button>
+                </a>";
     ?>
 </div>
 <?php
--- view/templates/product_overview.php
+++ view/templates/product_overview.php
@@ -13,9 +13,7 @@
 
     if($type == $selected){
 
-    //echo "<a id=\"product-page\" href=\"".$this->build_url('index.php', array('action' => 'product_page'))."&id=$id \"></a>";
-
-   echo "<a id=\"product-page\" href='index.php?action=product_page&id=$id'>";
+    echo "<a id=\"product-page\" href=\"".$this->build_url('index.php', array('action' => 'product_page', 'id' => $id))." \">";
 
     echo "<div class=product-container>";
 
@@ -28,9 +26,9 @@
     </div>
     <?php echo "<p class='price'>CHF $price</p>";
 
-    /*if ($this->controller->isAdmin()) {
+    if ($this->controller->isAdmin()) {
     echo "<br><a href=\"index.php?action=edit_product&id=$id\">Edit</a> | <a href=\"index.php?action=delete_product&id=$id\">Delete</a><br/>";
-    }*/
+    }
     ?>
 </div>
 <?php
